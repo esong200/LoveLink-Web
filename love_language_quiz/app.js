df2c2b0c9c92b8f590f88203b4d1739898a09040
Ethan Song <e.song200@gmail.com>
2023-04-18 18:43:19 -0700

    added an option to register from the login page

--- love_language_quiz/app.js
+++ love_language_quiz/app.js
@@ -114,11 +114,11 @@ app.post("/register", async (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-    if (req.query.loggedOut === "true") {
-      req.flash("success", "Logged out successfully!");
-    }
-    res.render("login");
-  });
+  if (req.query.loggedOut === "true") {
+    req.flash("success", "Logged out successfully!");
+  }
+  res.render("login");
+});
 
 app.post(
   "/login",
@@ -130,81 +130,78 @@ app.post(
 );
 
 app.get("/logout", (req, res) => {
-    req.session.destroy(err => {
-      if (err) {
-        res.redirect("/");
-      } else {
-        res.clearCookie("connect.sid");
-        res.redirect("/login?loggedOut=true");
-      }
-    });
+  req.session.destroy(err => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      res.clearCookie("connect.sid");
+      res.redirect("/login?loggedOut=true");
+    }
+  });
 });
-  
 
 // Quiz route
 app.get("/quiz", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("quiz");
-      } else {
-        req.flash("error", "You must be logged in to access the quiz.");
-        res.redirect("/login");
-      }
-    });
+  if (req.isAuthenticated()) {
+    res.render("quiz");
+  } else {
+    req.flash("error", "You must be logged in to access the quiz.");
+    res.redirect("/login");
+  }
+});
 
 app.get("/profile", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("profile");
-    } else {
-        req.flash("error", "You must be logged in to access the user profile.");
-        res.redirect("/login");
-    }
-    });
+  if (req.isAuthenticated()) {
+    res.render("profile");
+  } else {
+    req.flash("error", "You must be logged in to access the user profile.");
+    res.redirect("/login");
+  }
+});
 
 app.post("/quiz", async (req, res) => {
-    if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     const quizResults = {
-        name: req.body.name,
-        showLove: req.body.showLove,
-        preferredGift: req.body.preferredGift,
-        favoriteActivity: req.body.favoriteActivity,
-        communicationPreference: req.body.communicationPreference,
+      name: req.body.name,
+      showLove: req.body.showLove,
+      preferredGift: req.body.preferredGift,
+      favoriteActivity: req.body.favoriteActivity,
+      communicationPreference: req.body.communicationPreference,
     };
     try {
-        await User.updateOne(
+      await User.updateOne(
         { _id: req.user._id },
         { $push: { quizResults: quizResults } }
-        );
-        req.flash("success", "Quiz results saved successfully!");
-        res.redirect("/results");
+      );
+      req.flash("success", "Quiz results saved successfully!");
+      res.redirect("/results");
     } catch {
-        req.flash("error", "Failed to save quiz results. Please try again.");
-        res.redirect("/quiz");
+      req.flash("error", "Failed to save quiz results. Please try again.");
+      res.redirect("/quiz");
     }
-    } else {
-        req.flash("error", "You must be logged in to submit quiz results.");
-        res.redirect("/login");
-    }
+  } else {
+    req.flash("error", "You must be logged in to submit quiz results.");
+    res.redirect("/login");
+  }
 });
 
 app.get("/results", async (req, res) => {
-    if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     try {
-        const user = await User.findById(req.user._id);
-        res.render("results", { quizResults: user.quizResults });
+      const user = await User.findById(req.user._id);
+      res.render("results", { quizResults: user.quizResults });
     } catch {
-        req.flash("error", "Failed to load quiz results. Please try again.");
-        res.redirect("/quiz");
+      req.flash("error", "Failed to load quiz results. Please try again.");
+      res.redirect("/quiz");
     }
-    } else {
+  } else {
     req.flash("error", "You must be logged in to view quiz results.");
     res.redirect("/login");
-    }
+  }
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-    
- 
